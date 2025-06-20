3becb40a640e4e6109db359e652c67dac882b842
Smita-2004 <smita23tiwari.007@gmail.com>
2025-06-20 14:27:39 +0530

    updated

---
+++ project/javascript/file1.js
@@ -0,0 +1,17 @@
+// let studentName= "John";
+// let age= 20;
+// let promoted = true;
+// let score 
+// let city = null
+// console.log( studentName)
+// console.log(typeof studentName)
+// console.log(typeof age)
+// console.log(typeof promoted)
+// console.log(typeof score)
+// console.log(typeof city)
+
+
+let age = "10";
+console.log(typeof age);
+let ageNum = Number(age);
+console.log(typeof ageNum);
