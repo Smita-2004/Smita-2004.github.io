4d9e831bda8f9f0b03aa1f94696c130a7b2c513c
Smita-2004 <smita23tiwari.007@gmail.com>
2025-06-20 16:50:49 +0530

    updated

--- project/javascript/file9.js
+++ project/javascript/file9.js
@@ -11,10 +11,27 @@ const score = [2, 1,7, 5,3];
 // const newScore = [...score,score1];
 // console.log(newScore);
 
-const cart ={
-    1:5,
-    5:3,
+// const cart ={
+//     1:5,
+//     5:3,
 
-};
-const newCart=({...cart,2:9})
-console.log(newCart)
+// };
+// const newCart=({...cart,2:9})
+// console.log(newCart)
+
+let cart = {};
+const products = [
+    {id:1, name: "product1", price: 25},
+    {id:2, name: "product2", price: 50},
+    {id:3, name: "product3", price: 75},
+];
+// console.log(products[0])
+function addToCart(id){
+    cart = { ...cart,[id]:1};
+}
+
+
+addToCart(1);  
+addToCart(3);  
+addToCart(4);  
+console.log(cart);
