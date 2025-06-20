655b6d393bd4ff27d9ea81c4d009f4219e81d90f
Smita-2004 <smita23tiwari.007@gmail.com>
2025-06-20 15:14:36 +0530

    updated

---
+++ project/javascript/file3.js
@@ -0,0 +1,22 @@
+//////////////    Conditional Statements  /////////////////
+
+let a=10;
+let b =5;
+
+// if(a>b){
+//     console.log(`${a} is greater than ${b}`);
+// }
+// else if(b > a){
+//     console.log(`${b} is greater than ${a}`);
+// }
+// else{
+//     console.log("Both are equal");
+// }
+
+// a>b ? console.log("a is greater") : console.log("b is greater");
+
+// a > b && console.log("a is greater");
+
+let x;
+let res = x || 5;
+console.log(res);
---
+++ project/javascript/file5.js
@@ -0,0 +1,17 @@
+//////////////// regukar  function  //////////////
+
+// function greet(){
+//     console.log("Good Afternoon");
+// }
+// greet();
+
+
+ // IIFE method
+// (function greet(){
+//     console.log("Good Afternoon");
+// })();
+
+function add(a,b){
+    console.log(a+b);
+}
+add(4,5);
