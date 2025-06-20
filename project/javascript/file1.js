95234d22dff9616ac5acb20d1b80ae5bc3174c66
Smita-2004 <smita23tiwari.007@gmail.com>
2025-06-20 14:47:16 +0530

    updated

--- project/javascript/file1.js
+++ project/javascript/file1.js
@@ -1,3 +1,7 @@
+//////////////////////////////Intro introduction to Javascript///////////////////////////
+
+
+
 // let studentName= "John";
 // let age= 20;
 // let promoted = true;
@@ -11,7 +15,34 @@
 // console.log(typeof city)
 
 
-let age = "10";
-console.log(typeof age);
-let ageNum = Number(age);
-console.log(typeof ageNum);
+// let age = "10";
+// console.log(typeof age);
+// let ageNum = Number(age);
+// console.log(typeof ageNum);
+
+
+
+// let a= "10";
+// let b = 10;
+// console.log(a+b);
+
+
+// let a= "10";
+// let b = 10;
+// console.log(a*b);
+
+// let a= 2.10;
+// let b = 2.10;
+// console.log(a===b);
+
+// const c1 ="LPU";
+// c1 ="MU";
+// console.log(c1);
+
+
+// var stName = "John";
+// console.log(stName);
+
+// let stName = " Smita";
+// console.log(`My name is ${stName}`);
+
---
+++ project/javascript/file2.js
@@ -0,0 +1,37 @@
+/////////////Operators///////////////////////
+
+// let a=12;
+// let b= 3;
+// console.log(a+b)        // for +,-,* ,/
+// console.log(a%b);
+// console.log(a**b);  //power
+
+// a++;
+// console.log(a);
+
+// a+=5;
+// console.log(a);
+
+// let a= 5;
+// let b= 2;
+// console.log(a>b);
+
+
+// let a ="10";
+// let b = 10;
+// console.log(a==b);
+
+
+// let a ="10";
+// let b = 10;
+// console.log(a===b);
+
+
+// let a =5;
+// let b = 3;
+// let c = 1;
+// console.log(a>b && a>c);
+// console.log(a>b && a<c);
+// console.log(a>b || a<c);
+
+
