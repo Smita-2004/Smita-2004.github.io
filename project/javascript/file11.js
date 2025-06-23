36d61a145f1d14e553c91a2f7b8a8192a2c52ea5
Smita-2004 <smita23tiwari.007@gmail.com>
2025-06-23 16:53:53 +0530

    updated

---
+++ project/javascript/file11.js
@@ -0,0 +1,7 @@
+// import / export 
+
+import add ,{subtract} from "./calc.mjs"
+const res = add(4,5);
+console.log(res);
+const res1= subtract(10,9);
+console.log(res1);
---
+++ project/javascript/file13.js
@@ -0,0 +1,11 @@
+// spread operator
+
+let marks ={
+    maths: 95,
+};
+marks = {...marks,science: 54};
+console.log(marks)
+
+let students = ["harsh","Ananta","khushboo"];
+students = [...students,"Azam"]
+console.log(students)
