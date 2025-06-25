79bce4130728d9f3551fc8a91276387db0d61cec
Smita-2004 <smita23tiwari.007@gmail.com>
2025-06-25 15:13:22 +0530

    updated

--- mongodb/q3.js
+++ mongodb/q3.js
@@ -4,5 +4,18 @@ db.employees.find().limit(1)            // only 1st displayed
 db.employees.find().skip(1).limit(1)   // second one display
 db.employees.find({department:"IT"})
 db.employees.find({department:"IT"},{_id:0,name:1})
-db.employees.find({department:"IT"},{_id:0,name:1,salary:1})
+db.employees.find(
+    {department:{$eq: "IT"}},
+    {_id:0,name:1,salary:1}
+)
 
+
+db.employees.find(
+    {salary:{$gte: 3000}},  // gte : greater than or equal to
+    {_id:0,name:1,salary:1}
+)
+  // lte : less than or equal to
+db.employees.find(
+    {salary:{$lte: 3000}},
+    {_id:0,name:1,salary:1}
+)
