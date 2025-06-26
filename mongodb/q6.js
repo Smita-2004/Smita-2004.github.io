6187fe0e7fafba8445c5eda056ec71129c3c9711
Smita-2004 <smita23tiwari.007@gmail.com>
2025-06-26 15:41:36 +0530

    updated

--- mongodb/q6.js
+++ mongodb/q6.js
@@ -8,8 +8,71 @@ db.employess.find(
     { name:true }
 )
  
-// pipelines
+// each object is called as pipelines
 db.employees.aggregate([
-    {$match:{salary:{$gt:3000}}},
-    {$project:{name:1,salary:1}}
+    {$match:{salary:{$gt:2000}}},
+    {$project:{name:1,salary:1,location:1}},
+    {$sort:{name:1}},                  // -1 for descending order
+    {$limit: 1}
+])
+
+db.employees.aggregate([
+    {$project:{name:1,salary:1,location:1}},
+    
+])
+
+db.employees.updateMany(
+    {},
+    {$addToSet:{location:"FL"}},
+)
+
+//unwind -> display in separate line
+db.employees.aggregate([
+    {$project:{_id:0,name:1,location:1}},
+    {$unwind:"$location"},
+    
+])
+
+db.employees.aggregate([
+    {$project:{_id:0,EmpName:"$name"}},
+])
+
+db.employees.aggregate([
+    {$project:{_id:0,name:1,salary:1,bonus:{$multiply:["$salary",2]}}},
+])
+
+db.employees.aggregate([
+    {$project:{name:0}},
+])
+ 
+// display group wise salary
+db.employees.aggregate([
+    {$group:{_id:"$department",total:{$sum:"$salary"}}}
+]);
+
+// display total salary of all department
+db.employees.aggregate([
+    {$group:{_id:null,total:{$sum:"$salary"}}}
+]);
+
+// db.employees.insertOne([
+//   {
+//    _id: ObjectId('685bbf5cd25e3d3d5e748a61'),
+//    orderValue:1300,
+//   },
+// ]);
+
+db.employees.aggregate([
+    {$match: {salary:{$gt:2000}}},
+    {
+        $lookup:{
+        from:"$orders" ,
+        localField:" ",
+        foreignField:"_id",
+        as:"orders"
+    },
+},
+{$unwind:"$orders"},
+{$project:{name:1,slary:1,"orders.orderValue":1}},
+
 ])
