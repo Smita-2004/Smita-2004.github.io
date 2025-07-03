915f6462662545d7e8f5362b6d27d7b392d1245c
Smita-2004 <smita23tiwari.007@gmail.com>
2025-07-03 16:45:15 +0530

    updated

--- nodejs/nodeapp/index5.js
+++ nodejs/nodeapp/index5.js
@@ -3,7 +3,26 @@ const app = express()
 app.listen(8080,()=>{
     console.log("Server Started")
 });
-
+app.use(express.json());
+let products=[];
 app.post("/",(req,res)=>{
-    res.json(req.body);
+    const {id,name,price} = req.body;
+    const obj={
+        id:id,
+        name:name,
+        price:price,
+    };
+    products.push(obj);
+    res.json({message: "Product Created"});
+
 });
+app.get("/",(req,res)=>{
+    res.json(products);
+})
+
+app.delete("/:id",(req,res)=>{
+    const id = req.params.id;
+    products = products.filter(products=> products.id !== id);
+    res.json("Product Deleted");
+});
+
